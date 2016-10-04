982149c0d731d44927a3b71d3e7a6fccf0a63715
Vaibhav Sharma <vaib.sharma44@gmail.com>
2016-10-04 06:54:42 +0530

    first blood

--- fb_echobot/views.py
+++ fb_echobot/views.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import json, requests, random, re
 from pprint import pprint
-
+from config import API_KEY, PAGE_ACCESS_TOKEN,VERIFY_TOKEN
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -14,17 +14,12 @@ from bs4 import BeautifulSoup as BS
 
 
 # Create your views here.
-API_KEY = "00ed9d0a7061428493500c5d9bfb223e"
-PAGE_ACCESS_TOKEN = 'EAAIhB7QbAeEBAH0xBOZBSGX1mb0Gg0wMbiVZCEQuzAcXYgfpobDhgQ6EfK4pLwVoUn5vCfZCM6cCZCGgyOBjSGaBzNb6zDmVdZAaFx7ai2qCXmJswWw7tmxyUqnAyKCmw3Ga2dZCqTHyevArZA11dMuEI46xI3lRww3ZBHdoKOifoAZDZD'
-VERIFY_TOKEN = "vaibhavsharma"
-
-
-GREETINGS = {"name":1,"nice":0,"ok":0,"thank":0," how are you":2,"fine ":3," talk":4,"hi":1," hey":1," hello":1,"afternoon":6,"morning":7,"night":8,"hafiz":9,"doing":10,"pokemon":11,"are smart":12,"no":13}
+GREETINGS = {"name":1,"nice":0,"ok":0,"thank":0,"how are you":2,"fine ":3," talk":4,"hi":1,"hey":1,"hello":1,"afternoon":6,"morning":7,"night":8,"hafiz":9,"doing":10,"pokemon":11,"are smart":12,"no":13}
 reply=["My Pleasure My Master","Hi :) , My name is Stacko ! can answer all your tech question.","I am Fine my master. How are you ?","Okkay! if you need any help you just need to drop a message my master \n Khuda Hafiz! :)","Master you will only ask me tech questions","Hi :)","Good afternoon! My master :)","Good morning! My master","Good night! My master :)","khuda hafiz :)",">Sitting>Eating\n>Staring at the laptop screen\n>Typing\n>Breathing\n>Blinking\n>Thinking\nLol\nHow about you? ;) ","This might help you ! Made by my master :) http://enigmatic-basin-68757.herokuapp.com just give it a try . ","Thank you master ! this made my day :)","okkay ! then ?"]
 news_category = {'bollywood':'entertainment','film ':'entertainment','business':'buisness','market n':'buisness', 'entertainment':'entertainment', 'gam':'gaming', 'general new':'general','normal n':'general', 'science-and-nature':'science-and-nature','science':'science-and-nature','nature':'science-and-nature', 'sport':'sports','play':'sports', 'technology':'technology','gadgets':'technology','mobile':'technology'}
 
 def post_facebook_message(fbid, received_message):
-	print "fbid" , fbid
+	logg("@","fbid" + fbid ,"-27-")
 	if len(received_message) > 300 :
 		msg_length = len(received_message)/300
 		start = 0
@@ -38,7 +33,7 @@ def post_facebook_message(fbid, received_message):
                 "text": received_message[start:start+300]
             }
      })
-				else:
+				else :
 					response_msg1 = json.dumps(
         {"recipient":{"id":fbid}, 
             "message":{
@@ -50,27 +45,26 @@ def post_facebook_message(fbid, received_message):
 
 
 			except:
-				print "Post request Not working"
+				logg("&","Post request Not working","-53-")
 
 			start = start + 300
 			msg_length = msg_length -1
-	print received_message
+			print received_message
 	else:
-		response_msg1 = json.dumps(
+		try:
+			response_msg1 = json.dumps(
         {"recipient":{"id":fbid}, 
             "message":{
                 "text": received_message
             }
      })
-		try:
+			post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+			
 			status1 = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg1)
 			pprint(status1.json())
 
 		except:
-			print " RESPONSE NOT SEND "	
-
-
-
+			logg("@","RESPONSE NOT SEND","-72-")
 
 class fb(generic.View):
 	def get(self, request, *args, **kwargs):
@@ -93,7 +87,7 @@ class fb(generic.View):
 		 			try:
 		 				chat(message['sender']['id'], message['message']['text'])
 		 			except:
-		 				print "Message not send"
+		 				logg("#","Message not send","-95-")
 		 				pass
 		return HttpResponse()
 
@@ -118,35 +112,35 @@ def chat(fbid,message):
 				break
 			print url
 			if "http://stackoverflow.com/" in url:
-				print "entered"
+				logg("#"," ","-120-")
 				soverflowurl = url
 			else :
 				continue	
 
 		try:
-			print "Trying Stackoverflow URL"
+			logg("$","Trying Stackoverflow URL","-126-")
 			r = requests.get(soverflowurl).text
 			page = BS(r,"html.parser")
 			answer = page.find_all("div",class_ = "accepted-answer")
 			answer = answer[0].find_all("div",class_ = "post-text")
-			print "Post request made"
+			logg("!","Post request made","-131")
 			post_facebook_message(fbid,answer[0].text)
-			print answer[0].text
+			logg("@",answer[0].text,"-133-")
 			return
 		except IndexError:
 			try:
-				print "Index error"
+				logg("@","Index error","-137-")
 				r = requests.get(soverflowurl).text
 				page = BS(r,"html.parser")
 				answer = page.find_all("div",id="answers")
 				answer = answer[0].find_all("div",class_="answer")
 				answer = answer[0].find_all("div",class_="post-text")
-				print "index error post request made"
+				logg("#","index error post request made","-143-")
 				post_facebook_message(fbid,answer[0].text)
-				print answer[0].text
+				logg("$",answer[0].text ,"-145-")
 				return
 			except:
-				print "Extreme exception"
+				log("$","Extreme exception","-148-")
 				post_facebook_message(fbid,"Try giving me the exact problem! My master!")
 				return	
 
@@ -156,7 +150,7 @@ def news(fbid,message):
 		if category in message :
 			flag = 0
 			try :
-				print "Getting inside the try of news"
+				logg("$","News Funtion","-158-")
 				result = requests.get("https://newsapi.org/v1/sources?source=techcrunch&language=en&apikey="+API_KEY).json()
 				categ = news_category[category]
 				print categ
@@ -172,27 +166,27 @@ def news(fbid,message):
 								print "No response"	
 							return
 
-						print "Checking all the inputs"	
-						print xa['description'],xa['country'],xa['urlsToLogos']['medium'],xa['category'],xa['url']	
+						logg("%","Checking all the Inputs:","-174-")	
+						logg("%",xa['description'] +"\t" + xa['country'] +"\t"+ xa['urlsToLogos']+"\t" +['medium'] +"\t" +xa['category'] +"\t" +xa['url'] ,"-175")
 						post_facebook_template_message(fbid,xa['description'],xa['country'],xa['urlsToLogos']['medium'],xa['category'],xa['url'])
 						return
 			except :
-				print "template not send"
+				logg("&","Template not send:","-179-")
 				try:
 					post_facebook_message(fbid,"Sorry! which news category you want (please mention it with the suffix news) Thank you!")
 				except :
-					print "%s%s%s" %('*'*10+'\n',"Could not connect with the news API\n","'*'*10+'\n'")
+					logg("^","Could not connect with the news API:","-183-")
 			return 
 
 	if flag == 1:
 		try :
 			post_facebook_message(fbid,"Please try giving me the news category")
 		except:
-			print "%s%s%s" %('*'*10+'\n',"Could not connect with the news API\n","'*'*10+'\n'")
+			logg("#","API is not working ! might be server's Problem:","-190-")
 
 def post_facebook_template_message(fbid,description,country,logo,category,urls):
 
-	print "*************** post_facebbok_template_message() ***********"
+	logg("*","POST FACEBOOK TEMPLATE MESSAGE","-194-")
 
 	response_msg1 = json.dumps(
         {"recipient":{"id":fbid}, 
@@ -222,20 +216,19 @@ def post_facebook_template_message(fbid,description,country,logo,category,urls):
 })
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
 	try :
-		print "**********\n TRYING NEWS TEMPLATE ***********\n"
+		logg("*","TRYING NEWS TEMPLATE","-234-")
 		status1 = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg1)
 		pprint(status1.json())
 	except :
-		print "NEWS TEMPLATE FAILED"
+		logg("!","Template failed","-228-")
 		try :
 			print "POSTING DESCRIPTION NOT TEMPLATE"
 			post_facebook_message(fbid,description)
 		except :
-			print "NOTHING HAS WORKED OUT"
-			print "%s%s%s" %('*'*10+'\n',"Message was not send!",'*'*10+'\n')
+			logg("%","Template was not send","-23-")
 
 def videos(fbid,url="https://www.youtube.com/v/ej3ebj3F"):
-	print "************\nEntered inside the videos\n************"
+	logg("*","Video Section :"," -237-")
 	url = url +"\n"
 	url = url.decode('utf-8')
 	p = re.compile(ur'^https:\/\/www.youtube.com\/watch\?(?P<digit>\w+)=(?P<first_name>[0-9a-zA-Z]+)$')
@@ -254,14 +247,15 @@ def videos(fbid,url="https://www.youtube.com/v/ej3ebj3F"):
 })
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
 	try :
-		print "trying video message"
+		logg("%","Trying video message :"," -256-")
 		status1 = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg1)
 		pprint(status1.json())
 	except :
 		try :
-			print "************ VIDEO WAS NOT SEND ************"
+			logg("^","Video was not send :"," -261-")
 			post_facebook_message(fbid,"Please brief your choice!")
 		except :
-			print "EVERYTHING HAS FAILED MESSAGE WAS NOT SEND"
-			print "%s%s%s" %('*'*10+'\n',"Message was not send!",'*'*10+'\n')
+			logg("@","Message was not send :," , "-265-")
 
+def logg(symbol,text,lineno):
+	print symbol*10 + "\n" + "\t" + TEXT + "\n" + symbol*10 +"\n"+ "\t" + lineno*3 +"\n"
